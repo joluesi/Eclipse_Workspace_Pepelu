62872af225c4bd085f967401a16faabf5c234d39
dam18-27 <dam18-27@c06x24.iescierva.net>
2019-03-29 10:36:30 +0100

    añado PsP

--- RECUPERACION_PYTHON/ejercicios_clase/generarLista.py
+++ RECUPERACION_PYTHON/ejercicios_clase/generarLista.py
@@ -14,12 +14,12 @@ def GenerarLista():
             M[i].append(randint(0,9))
     return M
 
-'''
+
 def Semejantes(m1, m2):
-    if len(m1) == len(m2):
-        for i in range(len(m1)):
-            if len(m1[i]) == len(m2[i]):
-                semejantes = True
+    if len(m1) == len(m2): #si ambas matrices tienen igual número de sublistas
+        for i in range(len(m1)): # entonces, recorro cada una de las listas
+            if len(m1[i]) == len(m2[i]): # si la longitud de cada lista es igual
+                semejantes = True # son semejantes
             else:
                 semejantes = False
                 break
@@ -27,15 +27,15 @@ def Semejantes(m1, m2):
         semejantes = False
     
     return semejantes
-'''
+
  
 '''
 modifico Semejantes para quitar el break (práctica para examen)
 NOTA: CUANDO QUITO UN FOR POR UN WHILE, TENGO QUE PONER 3 CONDICIONES:
     - variable contador
-    - pregunta la longitud
+    - pregunta la longitud / condiciones del while
     - actualizar contador 
-'''
+
 def Semejantes(m1, m2):
     if len(m1) == len(m2):
         semejantes = True
@@ -49,7 +49,7 @@ def Semejantes(m1, m2):
         semejantes = False
     
     return semejantes
-    
+'''    
 
 
 
--- RECUPERACION_PYTHON/ejercicios_clase/matriz_aleatoria.py
+++ RECUPERACION_PYTHON/ejercicios_clase/matriz_aleatoria.py
@@ -1,12 +1,12 @@
 '''
 Created on 21/3/2019
-Generar matrices aleatorias de máximo 5 filas y columnas, con un máximo de 9 elementos
+Generar matrices aleatorias de máximo 5 filas y columnas, con elementos del 0 al 9
 @author: dam18-27
 '''
 
 from random import randint
 
-M = []
+M = list()
 
 for i in range (randint(1,5)):
     M.append(list())
